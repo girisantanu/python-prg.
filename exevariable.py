bddc78eff06ece5f4c0385dd673e857f106196fb
SANTANU GIRI <124768719+girisantanu@users.noreply.github.com>
2023-12-08 09:40:45 +0530

    Create exevariable.py

---
+++ exevariable.py
@@ -0,0 +1,16 @@
+#exercise of variable
+
+# • Add a person with name Tony Stark.
+# • Tony's age is 51 years old.
+# • Tony is a genius.
+
+
+Person_fname="Tony"
+Person_Lname="Stark"
+Age=53
+
+is_genius=True
+
+print(Person_fname,Person_Lname)
+print(Age)
+print(is_genius)
